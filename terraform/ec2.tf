1d7991ffe49918deda2b4f621bf899229f60150a
rafinder1 <rafpro33@gmail.com>
2025-06-24 21:08:36 +0200

    fix: add security group with allow ssh and http connection

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,6 +8,7 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.fastapi_sg.id]
 
   provisioner "remote-exec" {
     inline = [
